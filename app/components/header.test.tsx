ecf4b273ec682f54a68495423d5ada9aa94d53d1
Peter Brczán <peter.brczan.developer@gmail.com>
2023-09-19 18:58:22 +0200

    [feat #33]: fixed the failing tests and updated snapshots.

--- app/components/header.test.tsx
+++ app/components/header.test.tsx
@@ -1,6 +1,10 @@
 import { Header } from '@/app/components/header';
 import { render } from '@/app/utils/test-renderer';
 
+jest.mock('next/navigation', () => ({
+  usePathname: jest.fn().mockReturnValue('/en'),
+}));
+
 describe('[component]: header', () => {
   it('should match snapshot', () => {
     const { asFragment } = render(<Header />);
--- app/components/language-selector.test.tsx
+++ app/components/language-selector.test.tsx
@@ -1,6 +1,10 @@
 import { render } from '@/app/utils/test-renderer';
 import { LanguageSelector } from '@/app/components/language-selector';
 
+jest.mock('next/navigation', () => ({
+  usePathname: jest.fn().mockReturnValue('/en'),
+}));
+
 describe('[component]: language-selector', () => {
   it('should match snapshot', () => {
     const { asFragment } = render(<LanguageSelector />);
